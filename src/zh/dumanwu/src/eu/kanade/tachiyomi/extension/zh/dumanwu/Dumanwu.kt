175aa760c8abb0a74145c13373420805beb576ce
huaqianghua <huaqianghua@users.noreply.github.com>
2025-07-15 13:21:10 +0800

    Update Dumanwu.kt

--- src/zh/dumanwu/src/eu/kanade/tachiyomi/extension/zh/dumanwu/Dumanwu.kt
+++ src/zh/dumanwu/src/eu/kanade/tachiyomi/extension/zh/dumanwu/Dumanwu.kt
@@ -7,4 +7,33 @@ class Dumanwu : MMLook(
     "https://m.dumanwu1.com",
     "https://www.dumanwu1.com",
     useLegacyMangaUrl = false,
-)
+) {
+    override fun searchMangaRequest(page: Int, query: String, filters: FilterList): Request {
+        if (query.isNotBlank()) {
+            return GET("$baseUrl/search.php?keyword=$query", headers)
+        }
+        return super.searchMangaRequest(page, query, filters)
+    }
+
+    override fun searchMangaParse(response: Response): MangasPage {
+        if (response.request.url.toString().contains("search.php")) {
+            val entries = response.asJsoup().select(".mh-item").map { element ->
+                SManga.create().apply {
+                    url = element.selectFirst("a")!!.attr("href")
+                        .mustRemoveSurrounding("/comic/", "/")
+                    title = element.selectFirst(".mh-item-detali > h2")!!.text()
+                    author = element.selectFirst(".mh-item-detali > p")!!.ownText()
+                        .mustRemoveSurrounding("作者：", "")
+                    thumbnail_url = element.selectFirst("img")!!.attr("src")
+                }
+            }
+            return MangasPage(entries, entries.size == 24)
+        }
+        return super.searchMangaParse(response)
+    }
+
+    private fun String.mustRemoveSurrounding(prefix: String, suffix: String): String {
+        check(startsWith(prefix) && endsWith(suffix)) { "string doesn't match $prefix[...]$suffix" }
+        return substring(prefix.length, length - suffix.length)
+    }
+}
