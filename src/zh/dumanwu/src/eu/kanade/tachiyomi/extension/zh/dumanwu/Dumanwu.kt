015738307c8888bee5555f08c86c71404051be5a
huaqianghua <huaqianghua@users.noreply.github.com>
2025-07-15 15:10:25 +0800

    Update Dumanwu.kt

--- src/zh/dumanwu/src/eu/kanade/tachiyomi/extension/zh/dumanwu/Dumanwu.kt
+++ src/zh/dumanwu/src/eu/kanade/tachiyomi/extension/zh/dumanwu/Dumanwu.kt
@@ -2,10 +2,12 @@ package eu.kanade.tachiyomi.extension.zh.dumanwu
 
 import eu.kanade.tachiyomi.multisrc.mmlook.MMLook
 import eu.kanade.tachiyomi.network.GET
+import eu.kanade.tachiyomi.network.POST
 import eu.kanade.tachiyomi.source.model.FilterList
 import eu.kanade.tachiyomi.source.model.MangasPage
 import eu.kanade.tachiyomi.source.model.SManga
 import eu.kanade.tachiyomi.util.asJsoup
+import okhttp3.FormBody
 import okhttp3.Request
 import okhttp3.Response
 
@@ -17,20 +19,23 @@ class Dumanwu : MMLook(
 ) {
     override fun searchMangaRequest(page: Int, query: String, filters: FilterList): Request {
         if (query.isNotBlank()) {
-            return GET("$baseUrl/search.php?keyword=$query", headers)
+            val form = FormBody.Builder()
+                .add("k", query)
+                .build()
+            return POST("$baseUrl/s", body = form, headers = headers)
         }
         return super.searchMangaRequest(page, query, filters)
     }
 
     override fun searchMangaParse(response: Response): MangasPage {
-        if (response.request.url.toString().contains("search.php")) {
+        if (response.request.url.toString().contains("/s")) {
             val entries = response.asJsoup().select(".mh-item").map { element ->
                 SManga.create().apply {
                     url = element.selectFirst("a")!!.attr("href")
-                        .mustRemoveSurrounding("/comic/", "/")
+                        .removeSurrounding("/comic/", "/")
                     title = element.selectFirst(".mh-item-detali > h2")!!.text()
                     author = element.selectFirst(".mh-item-detali > p")!!.ownText()
-                        .mustRemoveSurrounding("作者：", "")
+                        .removeSurrounding("作者：", "")
                     thumbnail_url = element.selectFirst("img")!!.attr("src")
                 }
             }
@@ -39,7 +44,7 @@ class Dumanwu : MMLook(
         return super.searchMangaParse(response)
     }
 
-    private fun String.mustRemoveSurrounding(prefix: String, suffix: String): String {
+    private fun String.removeSurrounding(prefix: String, suffix: String): String {
         check(startsWith(prefix) && endsWith(suffix)) { "string doesn't match $prefix[...]$suffix" }
         return substring(prefix.length, length - suffix.length)
     }
