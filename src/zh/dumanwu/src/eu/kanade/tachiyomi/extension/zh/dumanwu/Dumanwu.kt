9fb9804dadac7bc5280468a9b039a60651a2f6a1
huzc <huaqianghua@users.noreply.github.com>
2025-07-16 19:33:48 +0800

    1

--- src/zh/dumanwu/src/eu/kanade/tachiyomi/extension/zh/dumanwu/Dumanwu.kt
+++ src/zh/dumanwu/src/eu/kanade/tachiyomi/extension/zh/dumanwu/Dumanwu.kt
@@ -1,10 +1,15 @@
 package eu.kanade.tachiyomi.extension.zh.dumanwu
 
 import eu.kanade.tachiyomi.multisrc.mmlook.MMLook
+import okhttp3.Headers
 
 class Dumanwu : MMLook(
     "读漫屋1",
     "http://m.dumanwu1.com",
     "http://www.dumanwu1.com",
     useLegacyMangaUrl = false,
-)
+) {
+    override fun headersBuilder(): Headers.Builder = super.headersBuilder()
+        .set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36")
+        .set("Referer", baseUrl)
+}
